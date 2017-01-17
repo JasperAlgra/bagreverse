692e387c44bff94024fc848a24dd464572863a00
Albert Drenth <albert@europetrack.eu>
2017-01-17 21:29:41 +0100

    andersom ook nodig (voor postcode+huisnummer zoeken bij locatieimport)

--- lib/helpers.php
+++ lib/helpers.php
@@ -159,3 +159,42 @@
                 'y' => $y);
         }
     }
+    
+    if(!function_exists('rd2wgs')) {
+        function rd2wgs($x, $y)
+        {
+                // Calculate WGS84 co�rdinates
+                $dX = ($x - 155000) * pow(10, -5);
+                $dY = ($y - 463000) * pow(10, -5);
+                $SomN = (3235.65389 * $dY) +
+                        (-32.58297 * pow($dX, 2)) +
+                        (-0.24750 * pow($dY, 2)) +
+                        (-0.84978 * pow($dX, 2) * $dY) +
+                        (-0.06550 * pow($dY, 3)) +
+                        (-0.01709 * pow($dX, 2) * pow($dY, 2)) +
+                        (-0.00738 * $dX) +
+                        (0.00530 * pow($dX, 4)) +
+                        (-0.00039 * pow($dX, 2) * pow($dY, 3)) +
+                        (0.00033 * pow($dX, 4) * $dY) +
+                        (-0.00012 * $dX * $dY);
+                $SomE = (5260.52916 * $dX) +
+                        (105.94684 * $dX * $dY) +
+                        (2.45656 * $dX * pow($dY, 2)) +
+                        (-0.81885 * pow($dX, 3)) +
+                        (0.05594 * $dX * pow($dY, 3)) +
+                        (-0.05607 * pow($dX, 3) * $dY) +
+                        (0.01199 * $dY) +
+                        (-0.00256 * pow($dX, 3) * pow($dY, 2)) +
+                        (0.00128 * $dX * pow($dY, 4)) +
+                        (0.00022 * pow($dY, 2)) +
+                        (-0.00022 * pow($dX, 2)) +
+                        (0.00026 * pow($dX, 5));
+
+                $Latitude = 52.15517440 + ($SomN / 3600);
+                $Longitude = 5.38720621 + ($SomE / 3600);
+
+                return Array(
+                        'lat' => $Latitude,
+                        'lon' => $Longitude);
+        }
+    }
