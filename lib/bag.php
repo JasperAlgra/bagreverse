9789ebb957bfdf1e991f1aa70ec956470b9d85a7
jasper <jasper@algra.io>
2017-03-09 15:26:55 +0100

    PEAR-be-gone

--- lib/bag.php
+++ lib/bag.php
@@ -1,225 +1,190 @@
 <?php
-    /**
-     * [Short description for file]
-     *
-     * [Long description for file (if any)...]
-     *
-     * @category   EuropeTrack 2.0
-     * @package    EuropeTrack 2.0
-     * @author     Jasper Algra <jasper@yarp-bv.nl>
-     * @copyright  (C)Copyright 2015 YARP B.V.
-     * @version    CVS: $Id:$
-     * @since      22-12-2015 / 17:27
-     */
+/**
+ * [Short description for file]
+ *
+ * [Long description for file (if any)...]
+ *
+ * @category   EuropeTrack 2.0
+ * @package    EuropeTrack 2.0
+ * @author     Jasper Algra <jasper@yarp-bv.nl>
+ * @copyright  (C)Copyright 2015 YARP B.V.
+ * @version    CVS: $Id:$
+ * @since      22-12-2015 / 17:27
+ */
 
-    namespace bag;
+namespace bag;
 
-    use DB;
-    use DOMDocument;
-    use Dotenv\Dotenv;
+use DOMDocument;
+use Dotenv\Dotenv;
+use PDO;
+use PDOException;
 
-    require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 
-    include 'DB.php';
-    include 'helpers.php';
+include 'helpers.php';
 
-    class bag
-    {
+class bag {
 
-        /** @var \DB */
-        var $DB;
+    /** @var PDO */
+    var $DB;
 
-        /** @var array for storing BAG results */
-        var $BAGResults;
+    /** @var array for storing BAG results */
+    var $BAGResults;
 
-        public function __construct() {
+    public function __construct() {
 
-            // Check for .env file
-            if (!file_exists('../.env')) {
-                die("No .env file found. Create one, see .env.example");
-            }
-
-            // Get config from .env file
-            $dotEnv = new Dotenv('..');
-            $dotEnv->load();
-
-            // Create connection to the postgres DB
-            $this->initDb();
-
+        // Check for .env file
+        if (!file_exists('../.env')) {
+            die("No .env file found. Create one, see .env.example");
         }
 
-        /**
-         * setup connection to postgres DB
-         * connects or dies
-         */
-        private function initDb() {
+        // Get config from .env file
+        $dotEnv = new Dotenv('..');
+        $dotEnv->load();
 
-            // Full DNS for connection to postgres
-            $DSN = Array(
-                'username' => env('DB_USERNAME'),
-                'password' => env('DB_PASSWORD'),
-                'phptype'  => 'pgsql',
-                'hostspec' => env('DB_HOST'),
-                'port'     => env('DB_PORT'),
-                'database' => env('DB_DATABASE'),
-            );
-
-
-            // Connect to postgres DB
-            $this->DB = DB::connect($DSN);
-
-            // Die on error
-            if (\PEAR::IsError($this->DB)) {
-                if (env('APP_DEBUG')) {
-                    echo 'Standard Message: ' . $this->DB->getMessage() . "\n";
-                    echo 'Standard Code: ' . $this->DB->getCode() . "\n";
-                    echo 'DBMS/User Message: ' . $this->DB->getUserInfo() . "\n";
-                    echo 'DBMS/Debug Message: ' . $this->DB->getDebugInfo() . "\n";
-                }
-
-                die("Unable to connect to the database: " . $this->DB->getMessage());
-            }
-
-
-            $this->DB->setFetchMode(DB_FETCHMODE_OBJECT);
-            $this->DB->query("SET DateStyle TO 'sql,european'");
-            $this->DB->query("SET client_encoding TO 'utf-8'");
-            $this->DB->query("SET search_path TO " . env('DB_SEARCH_PATH'));
-
-        }
-
-        /**
-         * @param array $latLngs
-         * @param int   $radius
-         *
-         * @return $this
-         */
-        public function search(array $latLngs, $radius = 500) {
-
-            // Max records per lat/lng
-            $maxRecords = 1;
-
-            // Lookup all lat/lngs
-            foreach ($latLngs as $latLng) {
-
-                // Convert lat/lon to RijksDriehoek
-                $rd = wgs2rd($latLng[0], $latLng[1]);
-                $x = $rd['x'];
-                $y = $rd['y'];
-
-                // Do lookup
-                $rows = $this->DB->getRow("SELECT * FROM nlx_adressen_voor_xy({$x},{$y},{$radius},{$maxRecords})");
-
-                // Add result to BAGResults var
-                $this->BAGResults[] = $rows;
-            }
-
-            // Return self
-            return $this;
-
-        }
-
-        public function searchPostal($postcode, $number) {
-
-        // SELECT ST_X(geopunt) as x, ST_Y(geopunt) as y FROM adres WHERE postcode = '$postcode' AND huisnummer = '$huisnummer' $toevoeging LIMIT 1;
-        // Max records per lat/lng
-            $maxRecords = 1;
-
-            $query = "SELECT ST_X(geopunt) as x, ST_Y(geopunt) as y, * FROM geo_adres WHERE postcode = '$postcode' AND huisnummer = '$number' LIMIT 1;";
-
-            $text = array(
-                'postode' => $postcode,
-                'huisnummer' => $number,
-            );
-
-//          return searchAddress($this->DB, $text);
-            // Do lookup
-            $row = $this->DB->getRow($query);
-            $latLng = rd2wgs($row->x, $row->y);
-            $row->lat = $latLng['lat'];
-            $row->lon = $latLng['lon'];
-            // Add result to BAGResults var
-            $this->BAGResults[] = $row;
-
-            // Return self
-            return $this;
-        }
-
-        public function outputXml() {
-
-            $xmlDoc = new DOMDocument('1.0', 'UTF-8');
-
-            /* create the root element of the xml tree */
-            $xmlRoot = $xmlDoc->createElement("reversegeocode");
-            $xmlRoot->setAttribute('timestamp', Date(DATE_RFC822));
-
-            /* append it to the document created */
-            $xmlRoot = $xmlDoc->appendChild($xmlRoot);
-
-            foreach ($this->BAGResults as $address) {
-
-                // Addressparts
-                $addressParts = $xmlDoc->createElement("addressparts");
-
-                // House number
-                $houseNumber = $address->huisnummer
-                    . ($address->huisletter ? $address->huisletter : NULL)
-                    . ($address->toevoeging ? " " . $address->toevoeging : NULL);
-                $addressPart = $xmlDoc->createElement('house_number',$houseNumber);
-                $addressParts->appendChild($addressPart);
-
-                // Road
-                $addressPart = $xmlDoc->createElement('road',$address->straatnaam);
-                $addressParts->appendChild($addressPart);
-
-                // City
-                $addressPart = $xmlDoc->createElement('city',$address->woonplaats);
-                $addressParts->appendChild($addressPart);
-
-                // State
-                $addressPart = $xmlDoc->createElement('state',$address->provincie);
-                $addressParts->appendChild($addressPart);
-
-                // Postcode
-                $addressPart = $xmlDoc->createElement('postcode',$address->postcode);
-                $addressParts->appendChild($addressPart);
-
-                // Country + country_code (always NL for BAG)
-                $addressPart = $xmlDoc->createElement('country','Nederland');
-                $addressParts->appendChild($addressPart);
-                $addressPart = $xmlDoc->createElement('country_code','nl');
-                $addressParts->appendChild($addressPart);
-
-//                foreach($address as $key=>$value) {
-//                    // Skip empty
-//                    if(is_null($value)) continue;
-//
-//                    // Create elements for each key/value pair
-//                    $addressPart = $xmlDoc->createElement($key, $value);
-//                    $addressParts->appendChild($addressPart);
-//                }
-
-                // Result 'header'
-                // Full text address
-                $stringAddress = $houseNumber . ", " . $address->straatnaam . ", " . $address->woonplaats . ", "
-                    . $address->provincie . ", " . $address->postcode . ", Nederland, nl";
-                $result = $xmlDoc->createElement("result", $stringAddress);
-
-                // Place id
-                $result->setAttribute('place_id', $address->gid);
-
-                // Add header to doc
-                $xmlRoot->appendChild($result);
-
-                // Append the addressParts to the XML doc
-                $xmlRoot->appendChild($addressParts);
-            }
-
-
-            /* get the xml printed */
-//            header("content-type: text/xml; charset=UTF-8");
-//            header("Access-Control-Allow-Origin: *");
-            echo $xmlDoc->saveXML();
-        }
+        // Create connection to the postgres DB
+        $this->initDb();
 
     }
+
+    /**
+     * setup connection to postgres DB
+     * connects or dies
+     */
+    private function initDb() {
+
+        // Full DNS for connection to postgres
+        $dsn = "pgsql:host=" . getenv('DB_HOST') . ";port=" . env('DB_PORT') . ";dbname=" . getenv('DB_DATABASE') . ";user=" . getenv('DB_USERNAME') . ";password=" . getenv('DB_PASSWORD');
+
+        // Setup PDO
+        try {
+            $this->DB = new PDO($dsn);
+            $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->DB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die('Error in Connecting to database: ' . $e->getMessage());
+        }
+
+        $this->DB->exec("SET DateStyle TO 'sql,european'");
+        $this->DB->exec("SET client_encoding TO 'utf-8'");
+        $this->DB->exec("SET search_path TO " . env('DB_SEARCH_PATH'));
+
+    }
+
+    /**
+     * @param array $latLngs
+     * @param int   $radius
+     *
+     * @return $this
+     */
+    public function search(array $latLngs, $radius = 500) {
+
+        // Max records per lat/lng
+        $maxRecords = 1;
+
+        // Lookup all lat/lngs
+        foreach ($latLngs as $latLng) {
+
+            // Convert lat/lon to RijksDriehoek
+            $rd = wgs2rd($latLng[0], $latLng[1]);
+            $x = $rd['x'];
+            $y = $rd['y'];
+
+            // Do lookup
+            $query = $this->DB->query("SELECT * FROM nlx_adressen_voor_xy({$x},{$y},{$radius},{$maxRecords})");
+            $rows = $query->fetchObject();
+            // Add result to BAGResults var
+            $this->BAGResults[] = $rows;
+        }
+
+        // Return self
+        return $this;
+
+    }
+
+    public function searchPostal($postcode, $number) {
+
+        $query = $this->DB->query("SELECT ST_X(geopunt) as x, ST_Y(geopunt) as y, * FROM geo_adres WHERE postcode = '$postcode' AND huisnummer = '$number' LIMIT 1;");
+        $row = $query->fetchObject();
+
+        // Do lookup
+        $latLng = rd2wgs($row->x, $row->y);
+        $row->lat = $latLng['lat'];
+        $row->lon = $latLng['lon'];
+
+        // Add result to BAGResults var
+        $this->BAGResults[] = $row;
+
+        // Return self
+        return $this;
+    }
+
+    public function outputXml() {
+
+        $xmlDoc = new DOMDocument('1.0', 'UTF-8');
+
+        /* create the root element of the xml tree */
+        $xmlRoot = $xmlDoc->createElement("reversegeocode");
+        $xmlRoot->setAttribute('timestamp', Date(DATE_RFC822));
+
+        /* append it to the document created */
+        $xmlRoot = $xmlDoc->appendChild($xmlRoot);
+
+        foreach ($this->BAGResults as $address) {
+
+            // Addressparts
+            $addressParts = $xmlDoc->createElement("addressparts");
+
+            // House number
+            $houseNumber = $address->huisnummer
+                . ($address->huisletter ? $address->huisletter : NULL)
+                . ($address->toevoeging ? " " . $address->toevoeging : NULL);
+            $addressPart = $xmlDoc->createElement('house_number', $houseNumber);
+            $addressParts->appendChild($addressPart);
+
+            // Road
+            $addressPart = $xmlDoc->createElement('road', $address->straatnaam);
+            $addressParts->appendChild($addressPart);
+
+            // City
+            $addressPart = $xmlDoc->createElement('city', $address->woonplaats);
+            $addressParts->appendChild($addressPart);
+
+            // State
+            $addressPart = $xmlDoc->createElement('state', $address->provincie);
+            $addressParts->appendChild($addressPart);
+
+            // Postcode
+            $addressPart = $xmlDoc->createElement('postcode', $address->postcode);
+            $addressParts->appendChild($addressPart);
+
+            // Country + country_code (always NL for BAG)
+            $addressPart = $xmlDoc->createElement('country', 'Nederland');
+            $addressParts->appendChild($addressPart);
+            $addressPart = $xmlDoc->createElement('country_code', 'nl');
+            $addressParts->appendChild($addressPart);
+
+            // Result 'header'
+            // Full text address
+            $stringAddress = $houseNumber . ", " . $address->straatnaam . ", " . $address->woonplaats . ", "
+                . $address->provincie . ", " . $address->postcode . ", Nederland, nl";
+            $result = $xmlDoc->createElement("result", $stringAddress);
+
+            // Place id
+            $result->setAttribute('place_id', $address->gid);
+
+            // Add header to doc
+            $xmlRoot->appendChild($result);
+
+            // Append the addressParts to the XML doc
+            $xmlRoot->appendChild($addressParts);
+        }
+
+
+        /* get the xml printed */
+//            header("content-type: text/xml; charset=UTF-8");
+//            header("Access-Control-Allow-Origin: *");
+        echo $xmlDoc->saveXML();
+    }
+
+}
--- www/reverse.php
+++ www/reverse.php
@@ -1,48 +1,43 @@
 <?php
-    /**
-     * [Short description for file]
-     *
-     * [Long description for file (if any)...]
-     *
-     * @category   EuropeTrack 2.0
-     * @package    EuropeTrack 2.0
-     * @author     Jasper Algra <jasper@yarp-bv.nl>
-     * @copyright  (C)Copyright 2015 YARP B.V.
-     * @version    CVS: $Id:$
-     * @since      22-12-2015 / 17:48
-     */
+/**
+ * [Short description for file]
+ *
+ * [Long description for file (if any)...]
+ *
+ * @category   EuropeTrack 2.0
+ * @package    EuropeTrack 2.0
+ * @author     Jasper Algra <jasper@yarp-bv.nl>
+ * @copyright  (C)Copyright 2015 YARP B.V.
+ * @version    CVS: $Id:$
+ * @since      22-12-2015 / 17:48
+ */
 
-    include('../lib/bag.php');
+include('../lib/bag.php');
 
-    $bag = new bag\bag();
+$bag = new bag\bag();
 
-    $latLngs = Array();
+$latLngs = Array();
 
-    // Get single lat/lon from GET
-    if (isset($_GET['lat']) AND isset($_GET['lon'])) {
-        $latLngs[] = Array($_GET['lat'], $_GET['lon']);
-    }
+// Get single lat/lon from GET
+if (isset($_GET['lat']) AND isset($_GET['lon'])) {
+    $latLngs[] = Array($_GET['lat'], $_GET['lon']);
+}
 
-    // Get array of lats/lons from GET or POST
-    if (isset($_GET['latLngs'])) {
-        $latLngs= json_decode($_GET['latLngs']);
-    } elseif (isset($_POST['latLngs'])) {
-        $latLngs= json_decode($_POST['latLngs']);
-    }
+// Get array of lats/lons from GET or POST
+if (isset($_GET['latLngs'])) {
+    $latLngs = json_decode($_GET['latLngs']);
+} elseif (isset($_POST['latLngs'])) {
+    $latLngs = json_decode($_POST['latLngs']);
+}
 
-//    die (json_encode(Array(
-//        Array(51.410433, 5.454866),
-//        Array(51.410433, 5.454866),
-//    )));
+$radius = 150;
+if (isset($_GET['radius'])) {
+    $radius = $_GET['radius'];
+}
 
-    $radius = 150;
-    if (isset($_GET['radius'])) {
-        $radius = $_GET['radius'];
-    }
-
-    // Search + output XML
-    header("content-type: text/xml; charset=UTF-8");
-    header("Access-Control-Allow-Origin: *");
-    $bag
-        ->search($latLngs, $radius)
-        ->outputXml();
+// Search + output XML
+header("content-type: text/xml; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+$bag
+    ->search($latLngs, $radius)
+    ->outputXml();
